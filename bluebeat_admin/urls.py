b3034af18ddba887696e2543f93aececbffa6fd4
Rotondwa <rotondwamk9864@gmail.com>
2024-09-02 02:03:30 +0200

    Save changes before rebase

--- bluebeat_admin/urls.py
+++ bluebeat_admin/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('applicant-view/<int:application_id>/', applicant_view, name="applicant-view"),
     path('user-management/',user_management, name="user-management"),
     path('send_status_email/', send_status_email_view, name='send_status_email'),
-    path('change_application_status/', update_application_status, name='change_application_status'),
+    path('change_application_status/', update_application_status, name='change_application_status')
 ]
 
